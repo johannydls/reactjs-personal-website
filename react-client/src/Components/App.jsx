180a6cc3b9929f3a54b5d5b0500dab996c8d0d32
Johanny LS <johanny.dls@gmail.com>
2019-05-26 13:44:46 -0300

    Add basics components

---
+++ react-client/src/Components/App.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { browserHistory } from 'react-router';
+
+//Components
+import HomePage from './HomePage';
+import NavBar from './HeaderComponent/NavBar';
+import Footer from './FooterComponent/Footer';
+
+class App extends Component {
+    render() {
+        return (
+            <Router>
+                <div>
+                    <NavBar />
+                    <Route name="home" exact path="/" component={HomePage} />
+                    <Footer />
+                </div>
+            </Router>
+        )
+    }
+}
+
+export default App;
